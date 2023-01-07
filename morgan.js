1fe149f48ce2927fa905acb68e9591a11b0bebed
Soumenlinux <121148149+Soumenlinux@users.noreply.github.com>
2023-01-07 14:30:52 +0530

    Add files via upload

--- morgan.js
+++ morgan.js
@@ -5,22 +5,28 @@ import db from './db.js';
 const app= express();
 app.use(express.json());
 app.set(db);
-const addData = async (data) => {
+const addData = async (data,userId) => {
 
     const db = getDatabase();
-    await set(ref(db, "logger/" ), {
+    await set(ref(db, "logger/" +userId), {
+        
       data:data
     });
   }
 
 // const logger = morgan('combined')
-
 app.use(morgan((tokens,req,res)=>{
- addData([tokens.method(req,res)])
-}))
+    addData([tokens.url(req,res)],Date.now())
+   }))
+
 app.get('/',(req,res)=>{
     res.send("hii")
 })
+
+app.get('/add',(req,res)=>{
+    res.send("hii add")
+})
+
 app.listen(8000, ()=>{
  
     console.log('server is running on http://localhost:8000')
